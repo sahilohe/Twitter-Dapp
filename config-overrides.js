1ee0a6c79cd7cf2e87590f22b2109ee59cfe6188
sahilohe <sahilohe97@gmail.com>
2023-06-16 08:48:32 +0000

    working code

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,8 @@
-const webpack = require('webpack'); 
+const webpack = require('webpack');
+
 module.exports = function override(config) {
   const fallback = config.resolve.fallback || {};
+
   Object.assign(fallback, {
     crypto: require.resolve("crypto-browserify"),
     stream: require.resolve("stream-browserify"),
@@ -9,7 +11,10 @@ module.exports = function override(config) {
     https: require.resolve("https-browserify"),
     os: require.resolve("os-browserify"),
     url: require.resolve("url"),
+    zlib: require.resolve("browserify-zlib"),
+    path: require.resolve("path-browserify"),
   });
+
   config.resolve.fallback = fallback;
   config.plugins = (config.plugins || []).concat([
     new webpack.ProvidePlugin({
--- src/constants.js
+++ src/constants.js
@@ -1,16 +1,16 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 export const APP_CONSTANTS = {
-  ALCHEMY_KEY:
-    "wss://sleek-rough-uranium.matic-testnet.discover.quiknode.pro/c7c16b97e7b45a469878ee260ad13a130010e26d/",
-  CONTRACT_ADDRESS: "0x03EEe58C59bCF52BcA8E77fb788370FE3FdA0Bb7",
-  OPENSEA_ASSETS_URL: "https://testnets.opensea.io/assets/mumbai",
-  CLIENT_ID:
-    "BKysQno7pBNfKwptl_uhB6Cd-dgEYnqgovvpneWj_N5desA7PmruvJENcG5rMcRqOZ2cP8LRFyYTDzjS6ceAaog",
-  RPC_TARGET:
-    "https://sleek-rough-uranium.matic-testnet.discover.quiknode.pro/c7c16b97e7b45a469878ee260ad13a130010e26d/",
-  APP_LOGO: "https://i.ibb.co/2gmPw9f/twitter-Dapp-Logo.jpg",
-  ADAPTER_TWITTER_CLIENT_VERIFIER: "twitter-dapp-poc-testnet",
-  ADAPTER_TWITTER_CLIENT_ID: "w24qlkJtNa7OdC75VIfcFYGAgJwacU7F",
-  AUTH0_DOMAIN: "https://dev-n79d9y-x.us.auth0.com",
-  BIRD_IMAGE: "https://i.ibb.co/42WNxRN/reshot-icon-bird-TL39-D5-P8-YH.png",
+  ALCHEMY_KEY:`${process.env.ALCHEMY_KEY}`,
+  CONTRACT_ADDRESS: `${process.env.CONTRACT_ADDRESS}`,
+  OPENSEA_ASSETS_URL: `https://testnets.opensea.io/assets/mumbai`,
+  CLIENT_ID:`${process.env.CLIENT_ID}`,
+  RPC_TARGET:`${process.env.RPC_TARGET}`,
+  APP_LOGO: `https://i.ibb.co/2gmPw9f/twitter-Dapp-Logo.jpg`,
+  ADAPTER_TWITTER_CLIENT_VERIFIER: `${process.env.ADAPTER_TWITTER_CLIENT_VERIFIER}`,
+  ADAPTER_TWITTER_CLIENT_ID: `${process.env.ADAPTER_TWITTER_CLIENT_ID}`,
+  AUTH0_DOMAIN: `${process.env.AUTH0_DOMAIN}`,
+  BIRD_IMAGE: `https://i.ibb.co/42WNxRN/reshot-icon-bird-TL39-D5-P8-YH.png`,
   REACT_APP_REFRESH_TIMER: 30,
 };
